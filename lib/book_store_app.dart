daf94d143bd404ff495c103fd2b1fa841a3a74aa
Ahmed Mohamed Elkazaz <112720975+ahmedelkazaz39@users.noreply.github.com>
2025-06-11 15:07:11 +0300

    create folders for setup

---
+++ lib/book_store_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BookStoreApp extends StatelessWidget {
+  const BookStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('BOOK STORE APP'),
+      ),
+    );
+  }
+}
